6edad1cb7852d94ef87d7e833e333a62fddfd8f4
urbeckmd <urbeckmd@centurylink.net>
2023-10-14 16:45:09 -0500

    laptop add guest form

--- src/Components/Add_Guest_Page/Add_Guest_Screen.js
+++ src/Components/Add_Guest_Page/Add_Guest_Screen.js
@@ -41,7 +41,115 @@ function Add_Guest_Screen() {
       <Nav_Bar loggedIn={true} navHeader={'Pinnacle Lake Day Passes'} />
       <div className="add_guest_screen_module_container">
 
-        <div className="laptop_add_guest_module"></div>
+        <div className="laptop_add_guest_module">
+          <div className="laptop_add_guest_invites_container">
+            <Accordion>
+              <Accordion.Item eventKey="0">
+                <Accordion.Header>Saturday, June 6, 2023</Accordion.Header>
+                <Accordion.Body>
+                  <Form>
+                    <Form.Check type='radio' className='radio_button' label="Matt Urbeck" defaultChecked />
+                    <hr />
+                    <Form.Check type='radio' className='radio_button' label="Tim Mehegan" defaultChecked />
+                    <hr />
+                    <Form.Check disabled type='radio' className='radio_button' label="Adam Osvath" value={true} />
+                  </Form>
+                </Accordion.Body>
+              </Accordion.Item>
+              <Accordion.Item eventKey="1">
+                <Accordion.Header>Sunday, June 14, 2024</Accordion.Header>
+                <Accordion.Body>
+                  <Form>
+                    <Form.Check type='radio' className='radio_button' label="Dave Urbeck" defaultChecked />
+                  </Form>
+                </Accordion.Body>
+              </Accordion.Item>
+            </Accordion>
+            <Button className='invite_new_guests_button'>Invite New Guests</Button>
+          </div>
+          <div className="laptop_add_guest_invites_guest_info_container">
+            <Form className='laptop_add_guest_form'>
+              <Form.Group className="mb-3">
+                <Form.Label className='laptop_add_guest_input_label laptop_add_guest_input_label_name float-start'>First and Last Name</Form.Label>
+                <Form.Control className='laptop_add_guest_input_field' type="text" value={guestName} onChange={(e) => setGuestName(e.target.value)} />
+                <Dropdown className='laptop_add_guest_dropdown'>
+                  <Dropdown.Toggle className='laptop_add_guest_dropdown_toggle' variant="outline-secondary" id="dropdown-basic">
+                  </Dropdown.Toggle>
+
+                  <Dropdown.Menu>
+                    <Dropdown.Item onClick={(e) => handleDropdownSelect(e)}>
+                      <div className="laptop_add_guest_dropdown_option_container">
+                        <div className="laptop_add_guest_dropdown_option_name">Matt Urbeck</div>
+                        <div className="laptop_add_guest_dropdown_option_number">(636) 439-9972</div>
+                      </div>
+                    </Dropdown.Item>
+                    <NavDropdown.Divider />
+                    <Dropdown.Item onClick={(e) => handleDropdownSelect(e)}>
+                      <div className="laptop_add_guest_dropdown_option_container">
+                        <div className="laptop_add_guest_dropdown_option_name">Adam Osvath</div>
+                        <div className="laptop_add_guest_dropdown_option_number">(636) 240-5332</div>
+                      </div>
+                    </Dropdown.Item>
+                    <NavDropdown.Divider />
+                    <Dropdown.Item onClick={(e) => handleDropdownSelect(e)}>
+                      <div className="laptop_add_guest_dropdown_option_container">
+                        <div className="laptop_add_guest_dropdown_option_name">Tim Mehegan</div>
+                        <div className="laptop_add_guest_dropdown_option_number">(314) 640-0239</div>
+                      </div>
+                    </Dropdown.Item>
+                    <NavDropdown.Divider />
+                    <Dropdown.Item onClick={(e) => handleDropdownSelect(e)}>
+                      <div className="laptop_add_guest_dropdown_option_container">
+                        <div className="laptop_add_guest_dropdown_option_name">Adam Osvath</div>
+                        <div className="laptop_add_guest_dropdown_option_number">(636) 240-5332</div>
+                      </div>
+                    </Dropdown.Item>
+                    <NavDropdown.Divider />
+                    <Dropdown.Item onClick={(e) => handleDropdownSelect(e)}>
+                      <div className="laptop_add_guest_dropdown_option_container">
+                        <div className="laptop_add_guest_dropdown_option_name">Adam Osvath</div>
+                        <div className="laptop_add_guest_dropdown_option_number">(636) 240-5332</div>
+                      </div>
+                    </Dropdown.Item>
+                    <NavDropdown.Divider />
+                    <Dropdown.Item onClick={(e) => handleDropdownSelect(e)}>
+                      <div className="laptop_add_guest_dropdown_option_container">
+                        <div className="laptop_add_guest_dropdown_option_name">Adam Osvath</div>
+                        <div className="laptop_add_guest_dropdown_option_number">(636) 240-5332</div>
+                      </div>
+                    </Dropdown.Item>
+                    <NavDropdown.Divider />
+                    <Dropdown.Item onClick={(e) => handleDropdownSelect(e)}>
+                      <div className="laptop_add_guest_dropdown_option_container">
+                        <div className="laptop_add_guest_dropdown_option_name">Adam Osvath</div>
+                        <div className="laptop_add_guest_dropdown_option_number">(636) 240-5332</div>
+                      </div>
+                    </Dropdown.Item>
+                  </Dropdown.Menu>
+                </Dropdown>
+              </Form.Group>
+              <Form.Group className="mb-3">
+                <Form.Label className='laptop_add_guest_input_label float-start'>Phone Number</Form.Label>
+                <Form.Control className='laptop_add_guest_input_field' type="text" value={guestNumber} onChange={(e) => setGuestNumber(e.target.value)} />
+              </Form.Group>
+              <Form.Group className="mb-3">
+                <Form.Label className='laptop_add_guest_input_label float-start'>Date of Visit</Form.Label>
+                <Form.Control className='laptop_add_guest_input_field' type="date" value={guestDateOfVisit} onChange={(e) => setGuestDateOfVisit(e.target.value)} />
+              </Form.Group>
+              <Form.Check type="checkbox"
+                className='laptop_add_guest_checkbox'
+                id="default-checkbox"
+                label="Save this guest"
+                onClick={(e) => setGuestSaved(e.target.checked)}
+                checked={guestSaved}
+              />
+              <Button className='laptop_add_guest_button' type='submit' onClick={(e) => handleSubmit(e)}>Add Guest</Button>
+            </Form>
+          </div>
+        </div>
+
+
+
 
         <div className="mobile_add_guest_module">
           <Button className='phone_logout_button' onClick={handleLogout}>
